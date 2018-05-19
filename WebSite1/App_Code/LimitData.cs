50099f116425e859d6f55a874d3864c28cb7af5a
DESKTOP-VBLUO8U\Huyen <huyenchu26@gmail.com>
2018-05-19 18:21:38 +0700

    no message

---
+++ WebSite1/App_Code/LimitData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+/// <summary>
+/// Summary description for LimitData
+/// </summary>
+public class LimitData
+{
+    public LimitData()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static Dictionary<string, string> extract_data_begin(string timestamp_str)
+    {
+        string[] date_format_list = {
+            "MM/dd/yyyy hh:mm:ss tt",
+            "M/dd/yyyy hh:mm:ss tt",
+            "MM/d/yyyy hh:mm:ss tt",
+            "M/d/yyyy hh:mm:ss tt",
+            "MM/dd/yyyy hh:m:ss tt",
+            "MM/dd/yyyy h:mm:ss tt",
+            "M/dd/yyyy h:mm:ss tt",
+            "MM/d/yyyy h:mm:ss tt",
+            "M/d/yyyy h:mm:ss tt"
+        };
+
+        string[] timestamp_part = timestamp_str.Split('-');
+        DateTime parsed_date = DateTime.Now;
+        foreach (string date_format in date_format_list)
+            if (DateTime.TryParseExact(timestamp_part[1], date_format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsed_date))
+                break;
+        timestamp_part[1] = parsed_date.ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
+        Dictionary<string, string> timestamp_recv = new Dictionary<string, string> { };
+        int i = 0;
+        foreach (string field in VehicleModel.Vehicle["timestamp_recv"])
+        {
+            timestamp_recv[field] = timestamp_part[i];
+            i += 1;
+        }
+        return timestamp_recv;
+    }
+
+
+    public static Dictionary<string, string> extract_data(string raw_date, char delim = ',', char quotechar = '"')
+    {
+        Dictionary<string, string> extracted_dict = new Dictionary<string, string>();
+        string[] splitted_data = raw_date.Split(delim);
+        int i = 0;
+        foreach (string field in VehicleModel.Vehicle["data"])
+        {
+            extracted_dict[field] = splitted_data[i].Trim(quotechar);
+            i++;
+        }
+        return extracted_dict;
+    }
+
+    public string[] parse_rfid_list(string rfid_list_str)
+    {
+        List<string> rfid_list = new List<string>();
+        Match m = Regex.Match(rfid_list_str, @"[a-fA-F\d]{1,24}");
+        while (m.Success)
+        {
+            rfid_list.Add(m.Value);
+            m = m.NextMatch();
+        }
+        return rfid_list.ToArray();
+    }
+
+
+    // convert to timestamp
+    public static String DateToTimestamp(String date) {
+
+        DateTime currentDate = DateTime.Now;
+        Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(DateTime.Now)).TotalSeconds;
+
+        return "";
+    }
+
+}
--- WebSite1/App_Code/WebService.cs
+++ WebSite1/App_Code/WebService.cs
@@ -24,60 +24,7 @@ public class WebService : System.Web.Services.WebService
         //InitializeComponent(); 
     }
 
-    private Dictionary<string, string> parse_receive_timestamp(string timestamp_str)
-    {
-        string[] date_format_list = {
-            "MM/dd/yyyy hh:mm:ss tt",
-            "M/dd/yyyy hh:mm:ss tt",
-            "MM/d/yyyy hh:mm:ss tt",
-            "M/d/yyyy hh:mm:ss tt",
-            "MM/dd/yyyy hh:m:ss tt",
-            "MM/dd/yyyy h:mm:ss tt",
-            "M/dd/yyyy h:mm:ss tt",
-            "MM/d/yyyy h:mm:ss tt",
-            "M/d/yyyy h:mm:ss tt"
-        };
-
-        string[] timestamp_part = timestamp_str.Split('-');
-        DateTime parsed_date = DateTime.Now;
-        foreach (string date_format in date_format_list)
-            if (DateTime.TryParseExact(timestamp_part[1], date_format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsed_date))
-                break;
-        timestamp_part[1] = parsed_date.ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
-        Dictionary<string, string> timestamp_recv = new Dictionary<string, string> { };
-        int i = 0;
-        foreach (string field in VehicleModel.Vehicle["timestamp_recv"])
-        {
-            timestamp_recv[field] = timestamp_part[i];
-            i += 1;
-        }
-        return timestamp_recv;
-    }
-
-    public string[] parse_rfid_list(string rfid_list_str)
-    {
-        List<string> rfid_list = new List<string>();
-        Match m = Regex.Match(rfid_list_str, @"[a-fA-F\d]{1,24}");
-        while (m.Success)
-        {
-            rfid_list.Add(m.Value);
-            m = m.NextMatch();
-        }
-        return rfid_list.ToArray();
-    }
-
-    private Dictionary<string, string> extract_data(string raw_date, char delim = ',', char quotechar = '"')
-    {
-        Dictionary<string, string> extracted_dict = new Dictionary<string, string>();
-        string[] splitted_data = raw_date.Split(delim);
-        int i = 0;
-        foreach (string field in VehicleModel.Vehicle["data"])
-        {
-            extracted_dict[field] = splitted_data[i].Trim(quotechar);
-            i++;
-        }
-        return extracted_dict;
-    }
+    
 
     //get list file from pathfile
     public string[] ProcessDirectory(string pathfile)
@@ -113,11 +60,12 @@ public class WebService : System.Web.Services.WebService
                 line_of_text = file.ReadLine();
                 if (line_of_text == null || line_of_text.Trim('\n', '\r').Length == 0)
                     break;
-                record["timestamp_recv"] = this.parse_receive_timestamp(line_of_text);
+                record["timestamp_recv"] = LimitData.extract_data_begin(line_of_text);
                 line_of_text = file.ReadLine();
                 if (line_of_text == null || line_of_text.Trim('\n', '\r').Length == 0)
                     break;
-                record["data"] = this.extract_data(line_of_text);
+                record["data"] = LimitData.extract_data(line_of_text);
+                file_data.Clear();
                 file_data.Add(record);
             } while (true);
             filestream.Close();
@@ -129,13 +77,56 @@ public class WebService : System.Web.Services.WebService
         }
         return listJson;
     }
+    
 
     // input khoảng thời gian
+    // [WebMethod]
+    public List<String> getHistory(String imei, String startDate, String endDate) {
+        List<String> listJson = new List<string>();
+        String[] imeiArray = ProcessDirectory(Config.PathFile);
+        for (int i = 0; i < imeiArray.Length; i++)
+        {
+            if (imeiArray[i].Equals(imei)) {
+                List<Dictionary<string, Dictionary<string, string>>> file_data = new List<Dictionary<string, Dictionary<string, string>>>();
+                var filestream = new System.IO.FileStream(Config.PathFile + imeiArray[i], System.IO.FileMode.Open);
+                var file = new System.IO.StreamReader(filestream);
+                string line_of_text;
+                do
+                // chua handle error
+                {
+                    Dictionary<string, Dictionary<string, string>> record = new Dictionary<string, Dictionary<string, string>>();
+                    line_of_text = file.ReadLine();
+                    if (line_of_text == null || line_of_text.Trim('\n', '\r').Length == 0)
+                        break;
+                    record["timestamp_recv"] = LimitData.extract_data_begin(line_of_text);
+                    line_of_text = file.ReadLine();
+                    if (line_of_text == null || line_of_text.Trim('\n', '\r').Length == 0)
+                        break;
+                    record["data"] = LimitData.extract_data(line_of_text);
+
+                    if (line_of_text.Split('-')[1].Split(' ')[0].Equals(startDate)) {
+
+                    }
+
+                    // ngày càng xa thì timestamp càng lớn: thời gian tính từ 
+                    file_data.Add(record);
+                } while (true);
+                filestream.Close();
+
+                listJson.Add(JsonConvert.SerializeObject(file_data));
+            }
+        }
+        return listJson;
+    }
+
+    // 
     [WebMethod]
-    public String getHistory(String startDate, String endDate) {
+    public Int32 DateToTimestamp()
+    {
+        DateTime currentDate = DateTime.Now;
+        Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(2018, 5, 18))).TotalSeconds;
 
-
-        return startDate + " - " + endDate;
+        return unixTimestamp;
     }
 }
 
